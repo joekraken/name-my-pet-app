80f4652579365f0cae846aabc9de49404fc60106
joekraken <joseph.kirkbride@gmail.com>
2023-05-09 15:10:16 -0400

    added onchange event to input element

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import { useState } from 'react'
 
 // home page
 export default function Home() {
-  // a counter stored in state
+  // state management variables
   const [count, setCounter] = useState(0)
+  const [animalInput, setAnimalInput] = useState('')
 
   return (
     <>
@@ -18,9 +19,21 @@ export default function Home() {
           <h3>Generate Pet Name</h3>
           <p>You've used this app {count} times</p>
           <form>
-            <input type='text' name='animal' placeholder='enter an animal'/>
+            <input 
+              type='text'
+              name='animal'
+              value={animalInput}
+              onChange={(e) => {
+                setAnimalInput(e.target.value)
+                console.log(animalInput)
+              }}
+              placeholder='enter an animal'
+              />
             <input type='submit' onClick={(e) => {
               e.preventDefault()
+              if (count == 10) {
+                return console.log('reached your limit')
+              }
               setCounter(count + 1)
             }}/>
           </form>
